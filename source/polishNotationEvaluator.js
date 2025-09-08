bac7e7e544d77d67e7f4393ce6081bb98899e766
marivan <vanyamaryshev421@gmail.com>
2025-09-09 00:05:10 +0300

    fix strange tabulations

--- source/polishNotationEvaluator.js
+++ source/polishNotationEvaluator.js
@@ -13,49 +13,49 @@
 function polishNotationEvaluator(polishExpression) {
     if (polishExpression == ''){
         console.warn("На вход пришла пустая строка!");
-        return NaN
+        return NaN;
     }
 
     if ((typeof(polishExpression) != 'string') || !polishExpression){
-        throw new Error("Неверный аргумент функции - необходимо передать строку")
+        throw new Error("Неверный аргумент функции - необходимо передать строку");
     }
     
     let stack = [];
 
     polishExpression.trim().split(' ').filter(el => el !== '').reverse().forEach(elem => {               
-            let number = Number(elem);
-            let result;
+        let number = Number(elem);
+        let result;
 
-            if (isNaN(number)) {
-                let a = stack.pop();
-                let b = stack.pop();
+        if (isNaN(number)) {
+            let a = stack.pop();
+            let b = stack.pop();
 
-                if (a === undefined || b === undefined) {
-                    throw new Error("Недостаточное количество операндов (аргументов операции)");
-                }
-                
-                if (elem === '+') {
-                    result = a + b;
-                } else if (elem === '-') {
-                    result = a - b;
-                } else if (elem === '*') {
-                    result = a * b;
-                } else if (elem === '/') {
-                    if (b === 0) {
-                        throw new Error("На ноль делить нельзя!");
-                    }
-                    result = a / b;
-                } else {
-                    throw new Error(`Неизвестный элемент строки ${elem}`);
-                }
-                stack.push(result);
+            if (a === undefined || b === undefined) {
+                throw new Error("Недостаточное количество операндов (аргументов операции)");
             }
-            else {
-                stack.push(number);
+
+            if (elem === '+') {
+                result = a + b;
+            } else if (elem === '-') {
+                result = a - b;
+            } else if (elem === '*') {
+                result = a * b;
+            } else if (elem === '/') {
+                if (b === 0) {
+                    throw new Error("На ноль делить нельзя!");
+                }
+                result = a / b;
+            } else {
+                throw new Error(`Неизвестный элемент строки ${elem}`);
             }
-        });
+            stack.push(result);
+        }
+        else {
+            stack.push(number);
+        }
+    });
     if (stack.length > 1) {
-        throw new Error('Ошибка итогового значения')
+        throw new Error('Ошибка итогового значения');
     }
-    return stack[0]
+    return stack[0];
 }
